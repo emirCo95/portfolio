d61029881e8ae9a9387bc92c2180e93332b5be58
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-11-11 13:16:45 -0500

    deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,10 @@ export default {
   theme: {
     extend: {
       colors: {
+        ashGray: '#B5CBB7',
+        cream: '#E4E9B2',
+        teaGreen: '#D2E4C4',
+        battleship: '#E7E08B',
         background: 'var(--background)',
         foreground: 'var(--foreground)',
       },
