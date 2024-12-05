c5f5713324f435a252f921120578d6d3bd5778b6
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-04 19:07:27 -0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import React from 'react';
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react';
-import { pacifico } from '@/app/ui/fonts';
+import { montserrat, pacifico } from '@/app/ui/fonts';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -27,27 +27,27 @@ const Navbar = () => {
           <X className="mr-10 md:hidden" onClick={() => setMenuOpen(false)} />
           <ul className=" z-10 rounded-md absolute top-20 right-0 w-[200px] h-[500px] flex flex-col justify-between items-center p-10 bg-ashGray md:hidden">
             <li
-              className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl`}
+              className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl`}
             >
               <Link href="/">Home</Link>
             </li>
             <li
-              className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl`}
+              className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl`}
             >
               <Link href="/about">About</Link>
             </li>
             <li
-              className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl`}
+              className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl`}
             >
               <Link href="/experience">Experience</Link>
             </li>
             <li
-              className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl`}
+              className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl`}
             >
               <Link href="/skills">Skills</Link>
             </li>
             <li
-              className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl`}
+              className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl`}
             >
               <Link href="/projects">Projects</Link>
             </li>
@@ -58,27 +58,27 @@ const Navbar = () => {
       )}
       <ul className="hidden md:mr-10 md:flex md:justify-between md:items-center md:w-[50%]">
         <li
-          className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
+          className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
         >
           <Link href="/">Home</Link>
         </li>
         <li
-          className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
+          className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
         >
           <Link href="/about">About</Link>
         </li>
         <li
-          className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
+          className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
         >
           <Link href="/experience">Experience</Link>
         </li>
         <li
-          className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
+          className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
         >
           <Link href="/skills">Skills</Link>
         </li>
         <li
-          className={`${pacifico.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
+          className={`${montserrat.className} hover:text-white hover:bg-black p-2 text-xl lg:text-2xl xl:text-3xl`}
         >
           <Link href="/projects">Projects</Link>
         </li>
